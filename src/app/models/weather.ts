deb6a4cf817f1c48faf25f7cf8c9ba8cea6580c9
AlvaroSlzDev <147247653+AlvaroSlzDev@users.noreply.github.com>
2026-05-02 18:06:24 +0200

    Add files via upload

---
+++ src/app/models/weather.ts
@@ -0,0 +1,24 @@
+export interface WeatherResponse {
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+  };
+  weather: [
+    {
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  wind: {
+    speed: number;
+  };
+  sys: {
+    country: string;
+  };
+}
+
